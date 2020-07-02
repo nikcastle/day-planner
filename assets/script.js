e925e9d5323556529bcd3d210da70f595d655f7f
Lady Stock <nikki.castlea@gmail.com>
2020-07-02 11:13:29 -0600

    psuedocoding added

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+//Assignment code
+var day = ""; //day of the week
+var date = ""; //date
+var time = ""; //current time
+
+//set display for current day of the week & date, add time as well
+
+//show timeblocks for standard business hours, 9:00 am - 5:00 pm
+
+//color code time blocks according to actual time, if it's 11:15 am, 11:00 hour will be red, 9:00 & 10:00 am will be grey, all later hours will be green
+
+//create text input area within each timeblock
+
+//create save button for each time block, this should save to local storage
+
+//when page is refreshed, saved events are still visible
